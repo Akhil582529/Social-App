307b765e7cc70859553e40a799d883d86471b05d
Akhil Suryan <akhilsuryan123@gmail.com>
2026-04-28 16:20:39 +0530

    Implement form handling using useState

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'; 
 
 const Login = () => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log("Login form Submitted");
+  }
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+
   return (
     <div className="signup-container">
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={handleSubmit}>
         <div>
           <input 
             type="email" 
             placeholder="Enter your Email" 
             name="email"
+            value={formData.email}
+            onChange={handleChange}
           />
         </div>
 
@@ -18,6 +40,8 @@ const Login = () => {
             type="password" 
             placeholder="Enter your Password" 
             name="password"
+            value={formData.password}
+            onChange={handleChange}
           />
         </div>
 
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.css';
 
 const Signup = () => {
+  
+ const [formData, setFormData] = useState({
+  name: '',
+  email: '',
+  password: ''
+ });
+
+ const handleChange = (e) =>{
+  const {name, value} = e.target;
+  setFormData({
+    ...formData,
+    [name]: value
+  });
+ };
+
+ const handleSubmit = (e) =>{
+  e.preventDefault();
+  console.log('Sign-up form Submitted');
+  console.log(formData);
+ }
+
   return (
     <div className="signup-container">
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={handleSubmit}>
         <div>
           <input 
             type="text" 
             placeholder="Enter your Name" 
             name="name"
+            value={formData.name}
+            onChange={handleChange}
           />
         </div>
 
@@ -18,6 +41,8 @@ const Signup = () => {
             type="email" 
             placeholder="Enter your Email" 
             name="email"
+            value={formData.email}
+            onChange={handleChange}
           />
         </div>
 
@@ -26,6 +51,9 @@ const Signup = () => {
             type="password" 
             placeholder="Enter your Password" 
             name="password"
+            value={formData.password}
+            onChange={handleChange}
+            
           />
         </div>
 
